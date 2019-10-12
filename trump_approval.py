8689fb611ed41002dee98cd3f9b778aa1c759f9b
limax <limax@umich.edu>
2019-10-12 14:26:58 -0400

    Add averages to trump_approval.py

--- trump_approval.py
+++ trump_approval.py
@@ -8,8 +8,20 @@ url = "https://projects.fivethirtyeight.com/trump-approval-ratings/approval.json
 data = urllib.request.urlopen(url)
 data = json.loads(data.read().decode('utf-8'))
 
+approve_sum = 0
+disapprove_sum = 0
+count = 0
+
 for dict in data:
-    if dict["date"] == today:
-        if dict["subgroup"] == "All polls":
+
+    if dict["subgroup"] == "All polls":
+        approve_sum = approve_sum + float(dict["approve_estimate"])
+        disapprove_sum = disapprove_sum + float(dict["disapprove_estimate"])
+        count = count + 1
+
+        if dict["date"] == today:
+            approve_avg = approve_sum / count
+            disapprove_avg = disapprove_sum / count
             file = open("trump_approval_rating.json", 'w')
-            file.write("{\"approve\": " + str(dict["approve_estimate"]) + ", " + "\"disapprove\": " + str(dict["disapprove_estimate"]) + "}")
+            file.write("{\"approve_avg\": " + str(approve_avg) + ", " + "\"disapprove_avg\": " + str(disapprove_avg) + ", ")
+            file.write("\"approve\": " + str(dict["approve_estimate"]) + ", " + "\"disapprove\": " + str(dict["disapprove_estimate"]) + "}")
