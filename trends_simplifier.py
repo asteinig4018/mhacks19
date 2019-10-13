23dc2b2fd195ae94b672d5cf0112ba80be2f94f8
experimex <limax2012@gmail.com>
2019-10-12 22:37:43 -0400

    Get headlines working

--- trends_simplifier.py
+++ trends_simplifier.py
@@ -1,25 +1,27 @@
 import json
+import time
 
 NUM_STORIES = 5
 
+# No extension in filename
 def simplify(filename):
-    with open(filename) as json_file:
+    with open(filename + ".json") as json_file:
         trendsDict = json.load(json_file)
 
     storiesDict = trendsDict["storySummaries"]["trendingStories"]
 
-    file = open(filename, 'w')
+    file = open(filename + "_simp.json", 'w')
     file.write("{")
 
     for x in range(NUM_STORIES):
         title = storiesDict[x]["articles"][0]["articleTitle"]
         link = storiesDict[x]["articles"][0]["url"]
 
-        file.write("\"" + str(x) + "\": {\"title\": \"" + title + "\", \"link\": \"" + link + "\"}")
+        file.write("\"a" + str(x) + "\": {\"title\": \"" + title + "\", \"link\": \"" + link + "\"}")
         if x != NUM_STORIES-1:
             file.write(",")
 
     file.write("}")
 
-simplify("trends_realtime.json")
-simplify("trends_sports.json")
+simplify("trends_realtime")
+simplify("trends_sports")
